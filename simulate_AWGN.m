912d756ee11415e9575cdbb3cfda33420ed8994d
underdeveloper <der.sulthana@gmail.com>
2022-12-18 12:59:50 +0700

    differentiated variables between the two files to make it easier to have a shared workspace

--- simulate_AWGN.m
+++ simulate_AWGN.m
@@ -6,51 +6,52 @@ clc;
 clear;
 close all;
 
-data_length = 1e6;
-ebno_db = 0:2:8;
-ebno_num = 10.^(ebno_db/10); % Note to myself: Eb/No is a power value so dB to numerical conversion is /10
+data_length_t1 = 1e6;
+ebno_t1_db = 0:2:8;
+ebno_t1_num = 10.^(ebno_t1_db/10); % Note to myself: Eb/No is a power value so dB to numerical conversion is /10
 
-carlo = 5; % Monte Carlo
+carlo_t1 = 5; % Monte Carlo
 
 % Preallocating memory (for performance reasons)
 % There's actually no need to do this if data_length is relatively small I just like doing it
-tx_data_binary = zeros(carlo, data_length);
-tx_data_bpsk = zeros(carlo, data_length);
-awgn_noise = zeros(carlo, data_length);
-rx_raw = zeros(carlo, data_length);
-rx_decoded = zeros(carlo, data_length);
-ber = zeros(carlo,1);
-mean_ber = zeros(length(ebno_db),1);
+tx_data_binary_t1 = zeros(carlo_t1, data_length_t1);
+tx_data_bpsk_t1 = zeros(carlo_t1, data_length_t1);
+awgn_noise_t1 = zeros(carlo_t1, data_length_t1);
+rx_raw_t1 = zeros(carlo_t1, data_length_t1);
+rx_decoded_t1 = zeros(carlo_t1, data_length_t1);
+ber_t1 = zeros(carlo_t1,1);
+mean_ber_t1 = zeros(length(ebno_t1_db),1);
 
-for k = 1:length(ebno_db)
-    for j = 1:carlo
+for k = 1:length(ebno_t1_db)
+    for j = 1:carlo_t1
         % Tx
-        tx_data_binary(j,:) = randi([0 1], data_length, 1); % Generates binary [0 1] code of size data_length x 1
-        tx_data_bpsk(j,:) = bpsk_modulate(tx_data_binary(j,:)); % Modulates bit stream into symbol stream with BPSK
+        tx_data_binary_t1(j,:) = randi([0 1], data_length_t1, 1); % Generates binary [0 1] code of size data_length x 1
+        tx_data_bpsk_t1(j,:) = bpsk_modulate(tx_data_binary_t1(j,:)); % Modulates bit stream into symbol stream with BPSK
 
         % Channel
-        awgn_noise(j,:) = (1/sqrt(2*ebno_num(k)))*(randn(data_length, 1)+1i*randn(data_length, 1)); % Generates noise according to Eb/No
+        awgn_noise_t1(j,:) = (1/sqrt(2*ebno_t1_num(k)))*(randn(data_length_t1, 1)+1i*randn(data_length_t1, 1)); % Generates noise according to Eb/No
 
         % Rx
-        rx_raw(j,:) = tx_data_bpsk(j,:) + awgn_noise(j,:); % Combines symbol stream with noise channel by simple addition
+        rx_raw_t1(j,:) = tx_data_bpsk_t1(j,:) + awgn_noise_t1(j,:); % Combines symbol stream with noise channel by simple addition
 %         rx_raw(j,:) = awgn(tx_data_bpsk(j,:),ebno_db(k)); % This is a lot better haha
-        rx_decoded(j,:) = bpsk_demodulate(rx_raw(j,:)); % Demodulates symbol stream into bitstream
-        ber(j) = sum(tx_data_binary(j,:)~=rx_decoded(j,:)) / data_length;
+        rx_decoded_t1(j,:) = bpsk_demodulate(rx_raw_t1(j,:)); % Demodulates symbol stream into bitstream
+        ber_t1(j) = sum(tx_data_binary_t1(j,:)~=rx_decoded_t1(j,:)) / data_length_t1;
     end
-    mean_ber(k) = mean(ber);
+    mean_ber_t1(k) = mean(ber_t1);
 end
 
 % Theoretical
 % ebno_theoretical_db = ebno_dB; % Uncomment if you want them to be the same
-ebno_theoretical_db = 0:0.1:10; % Comment if you want them to be the same
-ebno_theoretical_num = 10.^(ebno_theoretical_db/10);
-ber_theoretical = 0.5*(erfc(sqrt(ebno_theoretical_num)));
+ebno_theoretical_t1_db = 0:0.1:10; % Comment if you want them to be the same
+ebno_theoretical_t1_num = 10.^(ebno_theoretical_t1_db/10);
+ber_theoretical_t1 = 0.5*(erfc(sqrt(ebno_theoretical_t1_num)));
 
 % Plotting whopee
+clf(1);
 figure(1)
-semilogy(ebno_db, mean_ber,'-r','marker','o','color','#1ef7f4',LineWidth=2);
+semilogy(ebno_t1_db, mean_ber_t1,'-r','marker','o','color','#1ef7f4',LineWidth=2);
 hold on;
-semilogy(ebno_theoretical_db, ber_theoretical,'--g','color','#0988ba',LineWidth=2);
+semilogy(ebno_theoretical_t1_db, ber_theoretical_t1,'--g','color','#0988ba',LineWidth=2);
 grid on;
 xlim([0 30]);
 ylim([1e-6 1e0]);
@@ -58,3 +59,4 @@ xlabel("Eb/No (dB)");
 ylabel("Bit Error Rate");
 legend('Simulated AWGN channel', 'Theoretical AWGN channel');
 title("BER Performance in an AWGN channel");
+hold off;
--- simulate_fading.m
+++ simulate_fading.m
@@ -3,60 +3,62 @@
 % 2022.12.17
 
 clc;
-clear;
+% clear;
 % close all;
 
-data_length = 1e6;
-ebno_dB = 0:5:25;
-f_Doppler = 30; % Doppler shift frequency in Hz
-data_rate = 64e3; % in bits per second (bps)
+data_length_t2 = 1e6;
+ebno_t2_dB = 0:5:25;
+f_Doppler_t2 = 30; % Doppler shift frequency in Hz
+data_rate_t2 = 64e3; % in bits per second (bps)
 
-carlo = 10; % Monte Carlo
+carlo_t2 = 1; % Monte Carlo
 
 % Preallocating memory (for performance reasons)
 % There's actually no need to do this if data_length is relatively small I just like doing it
-tx_data_binary = zeros(carlo, data_length);
-tx_data_bpsk = zeros(carlo, data_length);
-awgn_noise = zeros(carlo, data_length);
-rx_raw = zeros(carlo, data_length);
-rx_decoded = zeros(carlo, data_length);
-ber = zeros(carlo,1);
-mean_ber = zeros(length(ebno_dB),1);
-fading_var = zeros(carlo, 1);
+tx_data_binary_t2 = zeros(carlo_t2, data_length_t2);
+tx_data_bpsk_t2 = zeros(carlo_t2, data_length_t2);
+awgn_noise_t2 = zeros(carlo_t2, data_length_t2);
+rx_raw_t2 = zeros(carlo_t2, data_length_t2);
+rx_decoded_t2 = zeros(carlo_t2, data_length_t2);
+ber_t2 = zeros(carlo_t2,1);
+mean_ber_t2 = zeros(length(ebno_t2_dB),1);
+fading_var_t2 = zeros(carlo_t2, 1);
+
 
 % Fading channel configuration
-fading_channel = (fading(data_length, f_Doppler, 1/data_rate))';
+fading_channel_t2 = (fading(data_length_t2, f_Doppler_t2, 1/data_rate_t2))';
 
-for k = 1:length(ebno_dB)
-    for j = 1:carlo
+for k = 1:length(ebno_t2_dB)
+    for j = 1:carlo_t2
         % Tx
-        tx_data_binary(j,:) = randi([0 1], data_length, 1); % Generates binary [0 1] code of size data_length x 1
-        tx_data_bpsk(j,:) = bpsk_modulate(tx_data_binary(j,:)); % Modulates bit stream into symbol stream with BPSK
+        tx_data_binary_t2(j,:) = randi([0 1], data_length_t2, 1); % Generates binary [0 1] code of size data_length x 1
+        tx_data_bpsk_t2(j,:) = bpsk_modulate(tx_data_binary_t2(j,:)); % Modulates bit stream into symbol stream with BPSK
 
         % Channel
-        fading_var(j) = sum(abs(fading_channel.*tx_data_bpsk(j,:)).^2/data_length);
-        awgn_noise(j,:) = 1/sqrt(2/fading_var(j))*(randn(data_length, 1)+1i*randn(data_length, 1));
+        fading_var_t2(j) = sum(abs(fading_channel_t2.*tx_data_bpsk_t2(j,:)).^2/data_length_t2);
+        awgn_noise_t2(j,:) = 1/sqrt(2/fading_var_t2(j))*(randn(data_length_t2, 1)+1i*randn(data_length_t2, 1));
 
         % Rx
-        rx_raw(j,:) = tx_data_bpsk(j,:).*fading_channel + 10^(-ebno_dB(k)/20)*awgn_noise(j,:); % Combines symbol stream with noise channel by simple addition
-        rx_decoded(j,:) = bpsk_demodulate(rx_raw(j,:)); % Demodulates symbol stream into bitstream
-        ber(j) = sum(tx_data_binary(j,:)~=rx_decoded(j,:)) / data_length;
+        rx_raw_t2(j,:) = tx_data_bpsk_t2(j,:).*fading_channel_t2 + 10^(-ebno_t2_dB(k)/20)*awgn_noise_t2(j,:); % Combines symbol stream with noise channel by simple addition
+        rx_decoded_t2(j,:) = bpsk_demodulate(rx_raw_t2(j,:)); % Demodulates symbol stream into bitstream
+        ber_t2(j) = sum(tx_data_binary_t2(j,:)~=rx_decoded_t2(j,:)) / data_length_t2;
     end
-    mean_ber(k) = mean(ber);
+    mean_ber_t2(k) = mean(ber_t2);
 end
 
 
 % Theoretical
 % ebno_theoretical_dB = ebno_dB; % Uncomment if you want them to be the same
-ebno_theoretical_dB = 0:2:30; % Comment if you want them to be the same
-ebno_theoretical = 10.^(ebno_theoretical_dB/10);
-ber_theoretical = 0.5*(1-(sqrt((ebno_theoretical)./(1+ebno_theoretical))));
+ebno_theoretical_t2_dB = 0:2:30; % Comment if you want them to be the same
+ebno_theoretical_t2 = 10.^(ebno_theoretical_t2_dB/10);
+ber_theoretical_t2 = 0.5*(1-(sqrt((ebno_theoretical_t2)./(1+ebno_theoretical_t2))));
 
 % Plotting whopee
+clf(2);
 figure(2)
-semilogy(ebno_dB, mean_ber,'-r','marker','x','color','#e04f3f',LineWidth=2);
+semilogy(ebno_t2_dB, mean_ber_t2,'-r','marker','x','color','#e04f3f',LineWidth=2);
 hold on;
-semilogy(ebno_theoretical_dB, ber_theoretical,'--g','color','#9e1708',LineWidth=2);
+semilogy(ebno_theoretical_t2_dB, ber_theoretical_t2,'--g','color','#9e1708',LineWidth=2);
 grid on;
 xlim([0 30]);
 ylim([1e-6 1e0]);
