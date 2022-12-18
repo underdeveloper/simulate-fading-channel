f46c6c31d29da300d888233a3016b382bdf5157f
underdeveloper <der.sulthana@gmail.com>
2022-12-18 23:17:52 +0700

    uogrhhhghg added power control support, though not sure i like this alg imp

---
+++ power_control.m
@@ -0,0 +1,22 @@
+function processed_signal = power_control(received_signal, symbol_rate, limit, control_power, control_sample_rate)
+arguments
+    received_signal (1,:) double {mustBeNumeric} % One-dimensional series of data (symbols) received by Rx, in Watts
+    symbol_rate double {mustBeNumeric} % Symbol rate, in sps
+    limit double {mustBeNumeric} % Power limit to where the control would bring signal back up again were it to dip below, in dBmW
+    control_power double {mustBeNumeric} % How much the power control has control over the signal, in dBmW
+    control_sample_rate double {mustBeNumeric} % Power control sampling rate, in Hz, must be larger than symbol_rate!
+
+end
+%     intermediate_signal = repelem(received_signal,floorDiv(control_sample_rate,symbol_rate));
+    processed_signal = received_signal;
+    limit_num = 10^(3+(limit/10)); % in Watts
+    iterate_limit = floorDiv(control_sample_rate,symbol_rate);
+
+    for m = 1:length(received_signal)
+        iterations = 1;
+        while (abs(processed_signal(m)) < limit_num && iterations <= iterate_limit)
+            disp(10*log10(abs(processed_signal(m))/1e-3));
+            processed_signal(m) = 10^(3+((10*log10(abs(processed_signal(m))/1e-3) + control_power)/10)) * exp(angle(processed_signal(m)));
+            iterations = iterations + 1;
+        end
+    end
--- simulate_fading_and_power_control.m
+++ simulate_fading_and_power_control.m
@@ -8,10 +8,12 @@ clc;
 clf;
 
 data_length_t3 = 1e6;
-ebno_t3_db = 10;
+ebno_t3_db = 0:5:25;
 f_Doppler_t3 = [15 30 180]; % Doppler shift frequency in Hz
-f_power_control_t3 = 1800; % Power control frequency in Hz
+f_power_control_t3 = 1800; % Power control sampling rate in Hz
 data_rate_t3 = 64e3; % in bits per second (bps)
+
+% carlo_t3 = 1; % Monte Carlo
 
 % Preallocating memory (for performance reasons)
 % There's actually no need to do this if data_length is relatively small I just like doing it
@@ -19,55 +21,63 @@ tx_data_binary_t3 = zeros(length(f_Doppler_t3), data_length_t3);
 tx_data_bpsk_t3 = zeros(length(f_Doppler_t3), data_length_t3);
 awgn_noise_t3 = zeros(length(f_Doppler_t3), data_length_t3);
 rx_raw_t3 = zeros(length(f_Doppler_t3), data_length_t3);
+rx_controlled_t3 = zeros(length(f_Doppler_t3), data_length_t3);
 rx_equalised_t3 = zeros(length(f_Doppler_t3), data_length_t3);
 rx_decoded_t3 = zeros(length(f_Doppler_t3), data_length_t3);
+ber_t3 = zeros(length(ebno_t3_db),length(f_Doppler_t3));
 fading_channel_t3 = zeros(length(f_Doppler_t3), data_length_t3);
-ber_t3 = zeros(1,length(f_Doppler_t3));
-% fading_var_t3 = ones(carlo_t3, 1);
+% fading_var_t3 = ones(length(f_Doppler_t3), 1);
 
+% System characteristics
+tx_power_t3 = 1; % Transmit power at Tx, in Watts
+target_ebno_t3_db = 10; 
 
 for m = 1:length(f_Doppler_t3)
-    % Tx
-    tx_data_binary_t3(m,:) = randi([0 1], data_length_t3, 1); % Generates binary [0 1] code of size data_length x 1
-    tx_data_bpsk_t3(m,:) = bpsk_modulate(tx_data_binary_t3(m,:)); % Modulates bit stream into symbol stream with BPSK
+    for k = 1:length(ebno_t3_db)
+        % Tx
+        tx_data_binary_t3(m,:) = randi([0 1], data_length_t3, 1); % Generates binary [0 1] code of size data_length x 1
+        tx_data_bpsk_t3(m,:) = tx_power_t3.*bpsk_modulate(tx_data_binary_t3(m,:)); % Modulates bit stream into symbol stream with BPSK
 
-    % Channel
-        % Fading channel configuration
-    % fading_channel_t3 = fading(data_length_t3, f_Doppler_t3, 1/data_rate_t3)';
-    % % To be honest, I still don't get what this function actually does.
-    % % Is this only small scale fading?  
-    fading_channel_t3(m,:) = fading2(data_length_t3, f_Doppler_t3(m), 1/data_rate_t3); % Using someone else's code
-%         fading_var_t3(m) = sum(abs(fading_channel_t3.*tx_data_bpsk_t3(m,:)).^2/data_length_t3); % Let's say no variance for now
-    awgn_noise_t3(m,:) = (1/sqrt(2))*(randn(data_length_t3, 1)+1i*randn(data_length_t3, 1)); % Generates noise according to Eb/No
-    rx_raw_t3(m,:) = tx_data_bpsk_t3(m,:).*fading_channel_t3(m,:) ...
-        + 10^(-ebno_t3_db/20)*awgn_noise_t3(m,:); % Combines symbol stream with fading channel with per-element multiplication, then noise channel by simple addition
+        % Channel
+            % Fading channel configuration
+        % fading_channel_t3 = fading(data_length_t3, f_Doppler_t3, 1/data_rate_t3)';
+        % % To be honest, I still don't get what this function actually does.
+        % % Is this only small scale fading?  
+        fading_channel_t3(m,:) = fading2(data_length_t3, f_Doppler_t3(m), 1/data_rate_t3); % Using someone else's code
+%         fading_var_t3(j) = sum(abs(fading_channel_t3.*tx_data_bpsk_t3(j,:)).^2/data_length_t3); % Let's say no variance for now
+        awgn_noise_t3(m,:) = (1/sqrt(2))*(randn(data_length_t3, 1)+1i*randn(data_length_t3, 1)); % Generates noise according to Eb/No
+        rx_raw_t3(m,:) = tx_data_bpsk_t3(m,:).*fading_channel_t3(m,:) + 10^(-ebno_t3_db(k)/20)*awgn_noise_t3(m,:); % Combines symbol stream with fading channel with per-element multiplication, then noise channel by simple addition
 
-    % Rx
-    rx_equalised_t3(m,:) = rx_raw_t3(m,:)./fading_channel_t3(m,:); % Equalizing according to fading channel estimation, for now assume Rx knows exactly what the channel characteristics are
-    rx_decoded_t3(m,:) = bpsk_demodulate(rx_equalised_t3(m,:)); % Demodulates symbol stream into bitstream
-    ber_t3(m) = sum(tx_data_binary_t3(m,:)~=rx_decoded_t3(m,:)) / data_length_t3;
+        % Rx
+        rx_controlled_t3(m,:) = power_control(rx_raw_t3(m,:),data_rate_t3,-30,0.5,f_power_control_t3);
+        rx_equalised_t3(m,:) = rx_controlled_t3(m,:)./fading_channel_t3(m,:); % Equalizing according to fading channel estimation, for now assume Rx knows exactly what the channel characteristics are
+        rx_decoded_t3(m,:) = bpsk_demodulate(rx_equalised_t3(m,:)); % Demodulates symbol stream into bitstream
+        ber_t3(k,m) = sum(tx_data_binary_t3(m,:)~=rx_decoded_t3(m,:)) / data_length_t3;
+    end
 end
 
-% 
+
 % % Theoretical
-% % ebno_theoretical_t3_dB = ebno_t3_dB; % Uncomment if you want them to be the same
-% ebno_theoretical_t3_db = 0:2:30; % Comment if you want them to be the same
-% ebno_theoretical_t3 = 10.^(ebno_theoretical_t3_db/10);
-% ber_theoretical_t3 = 0.5*(1-(sqrt((ebno_theoretical_t3)./(1+ebno_theoretical_t3))));
-% 
-% % Plotting whopee
-% figure(5)
-% semilogy(ebno_t3_db, ber_t3,'-r','marker','o','color','#e04f3f',LineWidth=2);
-% hold on;
-% semilogy(ebno_theoretical_t3_db, ber_theoretical_t3,'--g','color','#9e1708',LineWidth=1);
-% grid on;
-% xlim([0 30]);
-% ylim([1e-6 1e0]);
-% xlabel("Eb/No (dB)");
-% ylabel("Bit Error Rate");
-% legend('Simulated Rayleigh fading channel', 'Theoretical Rayleigh fading channel', 'Location', 'southeast');
-% legend boxoff;
-% title("BER Performance in a Rayleigh fading channel");
+% ebno_theoretical_t3_dB = ebno_t3_dB; % Uncomment if you want them to be the same
+ebno_theoretical_t3_db = 0:2:30; % Comment if you want them to be the same
+ebno_theoretical_t3 = 10.^(ebno_theoretical_t3_db/10);
+ber_theoretical_t3 = 0.5*(1-(sqrt((ebno_theoretical_t3)./(1+ebno_theoretical_t3))));
+
+% Plotting whopee
+figure(5)
+semilogy(ebno_t3_db, ber_t3(:,1),'-o','color','#22b800',LineWidth=1); % fd = 15 Hz
+hold on;
+semilogy(ebno_t3_db, ber_t3(:,2),'-o','color','#c49300',LineWidth=1); % fd = 30 Hz
+semilogy(ebno_t3_db, ber_t3(:,3),'-o','color','#bf1d00',LineWidth=1); % fd = 180 Hz
+semilogy(ebno_theoretical_t3_db, ber_theoretical_t3,'--g','color','#9e1708',LineWidth=1.5);
+grid on;
+xlim([0 30]);
+ylim([1e-6 1e0]);
+xlabel("Eb/No (dB)");
+ylabel("Bit Error Rate");
+legend('Fading channel with fD = 15 Hz', 'Fading channel with fD = 30 Hz', 'Fading channel with fD = 180 Hz', 'Theoretical BER', 'Location', 'southeast');
+legend boxoff;
+title("BER Performance in a Rayleigh fading channel");
 
 
 % Constellation mapping
@@ -89,3 +99,5 @@ xlabel('Real');
 ylabel('Imaginary');
 title("Constellation at Rx");
 hold off;
+
+disp("Done!");
